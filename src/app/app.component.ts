e133dfdb4db81c46f5db96554fd6b2dd0b37a321
Suraj Adhikari <suraj.adhikari@pdc01.lisnepal.com.np>
2019-09-07 08:08:47 +0545

    template driven form approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practise';
+  @ViewChild('refForm', {static: false}) signupForm: NgForm;
+  defaultQuestion = 'pet';
+  defaultMail = 'suraj.adhikari@lisnepal.com.np';
+  gender = ['male','female','Extra'];
+  user = {
+    username: '',
+    email: ' ',
+    gender: ' ',
+    answer: ' ',
+    secret: ' ',
+  };
+
+  submitted = false;
+
+
+  suggestName(){
+    const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+        userData:{
+          username: suggestedName,
+          email:'sjfsjh@dfhjfh.com'
+        },
+        secret:'pet',
+        answer: '',
+        gender: 'male',
+    });
+  }
+
+  OnSubmit(){
+   this.submitted = true;
+   this.user.username = this.signupForm.value.userData.username;
+   this.user.email = this.signupForm.value.userData.email;
+   this.user.gender = this.signupForm.value.gender;
+   this.user.answer = this.signupForm.value.answer;
+   this.user.secret = this.signupForm.value.secret;
+
+ this.signupForm.reset();
+
+  }
 }
